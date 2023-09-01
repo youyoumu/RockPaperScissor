e562e4c97bba4f55ace3be49167d9fcbdbec9db2
yym2 <19760884+youyoumu@users.noreply.github.com>
2023-09-01 17:06:16 +0700

    Created function randomNumber123
    it generate random number between 1 or 2 or 3

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,1 +1,10 @@
 console.log("Hello World")
+
+// generating random number between 1 or 2 or 3
+function randomNumber123() {
+    let number = 0;
+    number = Math.floor(Math.random() * 3) + 1;
+
+    return number;
+}
+console.log(randomNumber123())
